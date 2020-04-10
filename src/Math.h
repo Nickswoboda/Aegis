a734e8497049b2199edbd91a8b6b0a42200ed8b7
Nickswoboda <41607512+Nickswoboda@users.noreply.github.com>
2020-04-09 22:04:18 -0700

    add AABB and early-out collision detection

--- src/Math.h
+++ src/Math.h
@@ -31,4 +31,25 @@ namespace Aegis
 		float z = 0;
 		float w = 0;
 	};
+
+	struct AABB
+	{
+		AABB() = default;
+		AABB(float x, float y, float w, float h)
+			: pos{ x, y }, size{ w, h } {}
+		Vec2 pos;
+		Vec2 size;
+	};
+
+	bool AABBHasCollided(const AABB& aabb_1, const AABB& aabb_2)
+	{
+		if ((int)aabb_1.pos.x < aabb_2.pos.x + aabb_2.size.x &&
+			(int)aabb_1.pos.x + aabb_1.size.x > aabb_2.pos.x &&
+			(int)aabb_1.pos.y < aabb_2.pos.y + aabb_2.size.y &&
+			(int)aabb_1.pos.y + aabb_1.size.y > aabb_2.pos.y) {
+			return true;
+		}
+
+		return false;
+	}
 };
