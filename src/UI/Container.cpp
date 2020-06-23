650acfc40a51e70598d379ba3857e06cf70fa4ce
Nickswoboda <41607512+Nickswoboda@users.noreply.github.com>
2020-06-22 18:07:04 -0700

    improve containers

--- src/UI/Container.cpp
+++ src/UI/Container.cpp
@@ -1,20 +1,29 @@
 #include "Container.h"
+#include "../Renderer/Renderer.h"
 
 namespace Aegis{
-	Container::Container(AABB rect, int padding)
-		:rect_(rect), padding_(padding)
+	Container::Container(AABB rect, int padding, Alignment alignment)
+		:rect_(rect), padding_(padding), alignment_(alignment), bg_color_({0.0f, 0.0f, 1.0f, 0.4f})
 	{
 	}
+
+	void Container::Render()
+	{
+		DrawQuad(rect_.pos, rect_.size, bg_color_);
+	}
+
 	VContainer::VContainer(AABB rect, int padding, Alignment alignment)
-		:Container(rect, padding), alignment_(alignment)
+		:Container(rect, padding, alignment)
 	{
 		
 	}
-	void VContainer::AddWidget(UIWidget* widget)
+
+	void Container::AddWidget(UIWidget* widget)
 	{
 		widgets_.push_back(widget);
 		UpdateWidgets();
 	}
+
 	void VContainer::UpdateWidgets()
 	{
 		int pen_y = rect_.pos.y + padding_;
@@ -26,6 +35,7 @@ namespace Aegis{
 			pen_y += widget->rect_.size.y + padding_;
 		}
 	}
+
 	void VContainer::AlignWidget(UIWidget* widget, Alignment alignment)
 	{
 		switch (alignment){
@@ -44,6 +54,47 @@ namespace Aegis{
 			{
 				int right_edge = rect_.pos.x + rect_.size.x;
 				widget->rect_.pos.x = right_edge - widget->rect_.size.x;
+				break;
+			}
+		}
+	}
+
+	HContainer::HContainer(AABB rect, int padding, Alignment alignment)
+		:Container(rect, padding, alignment)
+	{
+
+	}
+
+	void HContainer::UpdateWidgets()
+	{
+		int pen_x = rect_.pos.x + padding_;
+
+		for (auto& widget : widgets_){
+			widget->rect_.pos.x = pen_x;
+
+			AlignWidget(widget, alignment_);
+			pen_x += widget->rect_.size.x + padding_;
+		}
+	}
+
+	void HContainer::AlignWidget(UIWidget* widget, Alignment alignment)
+	{
+		switch (alignment){
+			case Alignment::Center:
+			{
+				int height_diff = rect_.size.y - widget->rect_.size.y;
+				widget->rect_.pos.y = rect_.pos.y + (height_diff / 2);
+				break;
+			}
+			case Alignment::Top: 
+			{
+				widget->rect_.pos.y = rect_.pos.y;
+				break;
+			}
+			case Alignment::Bottom:
+			{
+				int bottom_edge = rect_.pos.y + rect_.size.y;
+				widget->rect_.pos.y = bottom_edge - widget->rect_.size.y;
 				break;
 			}
 		}
--- src/UI/Container.h
+++ src/UI/Container.h
@@ -1,39 +1,56 @@
 #pragma once
 
 #include "UIWidget.h"
+#include "../Math/Vec4.h"
 
 namespace Aegis{
 	class Container
 	{
 	public:
-		Container(AABB rect_, int padding);
+		enum Alignment
+		{
+			Top,
+			Bottom,
+			Left,
+			Right,
+			Center
+		};
+		Container(AABB rect_, int padding, Alignment alignment);
 		virtual ~Container(){} 
-		virtual void AddWidget(UIWidget* widget) = 0;
+		void AddWidget(UIWidget* widget);
 		virtual void UpdateWidgets() = 0;
+		virtual void AlignWidget(UIWidget* widget, Alignment alignment) = 0;
+		void Render();
 
 
 		std::vector<UIWidget*> widgets_;
 		AABB rect_;
 		int padding_;
-
+		Alignment alignment_;
+		Vec4 bg_color_;
 	};
 
 	class VContainer : public Container
 	{
 	public:
-		enum class Alignment
-		{
-			Right,
-			Left,
-			Center,
-		};
 		VContainer(AABB rect, int padding, Alignment alignment);
 		~VContainer() {}
 
-		virtual void AddWidget(UIWidget* widget) override;
 		virtual void UpdateWidgets() override;
-		void AlignWidget(UIWidget* widget, Alignment alignment);
+		virtual void AlignWidget(UIWidget* widget, Alignment alignment) override;
 
-		Alignment alignment_;
+	};
+
+	class HContainer : public Container
+	{
+	public:
+
+		HContainer(AABB rect, int padding, Alignment alignment);
+		~HContainer() {}
+
+		virtual void UpdateWidgets() override;
+		virtual void AlignWidget(UIWidget* widget, Alignment alignment) override;
+
 	};
 }
+
