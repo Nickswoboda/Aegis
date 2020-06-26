3fe107fbc2200b72a768360aa3e1f11785f23404
Nickswoboda <41607512+Nickswoboda@users.noreply.github.com>
2020-06-25 21:04:58 -0700

    add ability to add textures for each state of the button

--- src/UI/Button.cpp
+++ src/UI/Button.cpp
@@ -16,37 +16,70 @@ namespace Aegis {
 	
 	bool Button::IsPressed(int action)
 	{
-		//Only return true if mouse was both pressed and released within button rect
-		if (PointInAABB(Application::GetWindow().GetMousePos(), rect_)) {
-			if (action == AE_BUTTON_PRESS) {
-				pressed_ = true;
-			}
-			else if (action == AE_BUTTON_RELEASE && pressed_) {
-				pressed_ = false;
-				return true;
-			}
+		//button must be pressed and released while hovering in ordering for it to be considered to be pressed
+		if (action == AE_BUTTON_PRESS) {
+			state_ = State::Pressed;
 		}
-
+		else if (action == AE_BUTTON_RELEASE && hovered_ && state_ == State::Pressed) {
+			state_ = State::Hovered;
+			return true;
+		}
+		else {
+			state_ = State::Normal;
+		}
 		return false;
 	}
 
 	void Button::Render(float delta_time)
 	{
-		DrawQuad(rect_.pos, rect_.size, color_);
+		if (textures_[state_]){
+			DrawQuad(rect_.pos, rect_.size, textures_[state_]);
+		}
 
 		if (!text_.empty()){
-		Renderer2D::SetFont(font_);
-		DrawText(text_, rect_.pos, { 1.0, 1.0f, 1.0f, 1.0f });
+			Renderer2D::SetFont(font_);
+			DrawText(text_, rect_.pos, { 1.0, 1.0f, 1.0f, 1.0f });
 		}
 	}
 
 	void Button::OnEvent(Event& event)
 	{
+		auto mouse_movement = dynamic_cast<MouseMoveEvent*>(&event);
+		if (mouse_movement){
+			if (PointInAABB(Vec2(mouse_movement->x_pos_, mouse_movement->y_pos_), rect_)) {
+				hovered_ = true;
+				if (state_ != State::Pressed){
+					state_ = State::Hovered;
+				}
+			}
+			else{
+				hovered_ = false;
+				if (state_ == State::Hovered){
+					state_ = State::Normal;
+				}
+			}
+				
+			return;
+		}
 		auto click = dynamic_cast<MouseClickEvent*>(&event);
 		if (click) {
 			if (IsPressed(click->action_)) {
 				callback_();
 				event.handled_ = true;
+			}
+		}
+	}
+
+	void Button::SetStateTexture(State state, std::shared_ptr<Texture> texture)
+	{
+		textures_[state] = texture;
+
+		if (state_ == State::Normal){
+			if (textures_[State::Hovered] == nullptr){
+				textures_[State::Hovered] = texture;
+			}
+			if (textures_[State::Pressed] == nullptr){
+				textures_[State::Pressed] = texture;
 			}
 		}
 	}
--- src/UI/Button.h
+++ src/UI/Button.h
@@ -4,12 +4,21 @@
 #include "../Math/AABB.h"
 #include "../Math/Vec4.h"
 #include "../Font.h"
+
+#include "array"
 #include <functional>
 
 namespace Aegis {
 	class Button : public Widget
 	{
 	public:
+		enum State
+		{
+			Normal,
+			Pressed,
+			Hovered,
+			NumStates
+		};
 		Button(const std::string& text, std::function<void()> callback);
 		Button(AABB rect, const std::string& text, std::function<void()> callback);
 
@@ -17,9 +26,11 @@ namespace Aegis {
 		void OnEvent(Event& event) override;
 		void Render(float delta_time = 0.0f) override;
 
-		Vec4 color_ = { 0.2, 0.4, 0.9, 1.0 };
+		void SetStateTexture(State state, std::shared_ptr<Texture> texture);
+		std::array<std::shared_ptr<Texture>, NumStates> textures_;
 		std::string text_;
-		bool pressed_ = false;
+		bool hovered_ = false;
 		std::function<void()> callback_;
+		State state_ = State::Normal;
 	};
 }
