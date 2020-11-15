6db22b0424b8b0953c660316c517c41d044d2073
Nick Swoboda <nickswoboda@gmail.com>
2020-11-15 15:01:35 -0800

    Clean sandbox cpp

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -5,142 +5,33 @@
 
 #include <glm/glm.hpp>
 
-class MenuScene : public Aegis::Scene
-{
-public:
-	MenuScene()
-	{ 
-	}
-	void Update() override
-	{
-		
-	}
-	void Render(float delta_time) override
-	{
-		Aegis::Renderer2D::BeginScene(camera_.view_projection_matrix_);
-		Aegis::RendererClear();
-
-		Aegis::DrawQuad({ 200, 200 }, { 100, 100 }, { 1.0, 0.0f, 1.0f, 1.0f });
-
-		Aegis::DrawText(text_, { 0, 400 }, { 1.0f, 1.0f, 1.0f, 1.0f });
-		Aegis::DrawText("Same Line", { (float)text_width, 400 }, { 1.0f, 1.0f, 1.0f, 1.0f });
-		//Aegis::DrawQuad({ 100.0f, 100.0f }, { (float)Aegis::Renderer2D::GetFont().atlas_.width_, (float)Aegis::Renderer2D::GetFont().atlas_.height_ }, std::make_shared <Aegis::Texture>(Aegis::Renderer2D::GetFont().atlas_), { 1.0, 1.0f, 1.0f, 1.0f });
-
-		Aegis::Renderer2D::EndScene();
-	}
-
-	void OnEvent(Aegis::Event& event) override
-	{
-		auto key_event = dynamic_cast<Aegis::KeyEvent*>(&event);
-		if (key_event && key_event->action_ == AE_BUTTON_PRESS) {
-			if (key_event->key_ == AE_KEY_ENTER) {
-				Aegis::Application::GetWindow().CenterOnScreen();
-			}
-			if (key_event->key_ == AE_KEY_A) {
-				Aegis::Application::GetWindow().SetSize(200, 200);
-			}
-			if (key_event->key_ == AE_KEY_S) {
-				Aegis::Application::GetWindow().SetPos(20, 25);
-			}
-		}
-		auto mouse_event = dynamic_cast<Aegis::MouseClickEvent*>(&event);
-
-	}
-
-	std::string text_;
-	int text_width = 0;
-};
-
-
 class Sandbox : public Aegis::Scene
 {
 public:
 	Sandbox()
 	{
-		smiley_ = Aegis::TextureManager::Load("assets/textures/smiley.png");
-		container_ = Aegis::TextureManager::Load("assets/textures/container.jpg");
+		ui_layer_ = std::make_unique<Aegis::UILayer>();
+		auto dialog_ = ui_layer_->AddWidget<Aegis::Dialog>("You Lose. Try Again?", Aegis::Vec2(300, 300), Aegis::Vec2(400, 400));
+		dialog_->SetAcceptedCallback([](){std::cout << "Accepted";});
+		dialog_->SetRejectedCallback([](){std::cout << "Rejected";});
 
-		fonts_.emplace_back(std::make_shared<Aegis::Font>("assets/fonts/Roboto-Regular.ttf", 16));
-		fonts_.emplace_back(std::make_shared<Aegis::Font>("assets/fonts/WorkSans-Regular.ttf", 16));
+
 	}
 
 	void Update() override
 	{
-		Aegis::Vec2 a(1, 0);
-		Aegis::Vec2 b(0, 1);
-		Aegis::Vec2 c(0, 1);
-
-		bool same = b != b;
-		bool diff = a != c;
-		x_pos_ += x_vel_;
 	}
-	void OnEvent(Aegis::Event& event)
+	void OnEvent(Aegis::Event& event) override
 	{
-		auto key_event = dynamic_cast<Aegis::KeyEvent*>(&event);
-		if (key_event) {
-			if (key_event->key_ == AE_KEY_A) {
-				time_step_ *= 2.0f;
-				Aegis::Application::SetTimeStep(time_step_);
-			}
-			else if (key_event->key_ == AE_KEY_S) {
-				time_step_ /= 2.0f;
-				Aegis::Application::SetTimeStep(time_step_);
-			}
-			else if (key_event->key_ == AE_KEY_F) {
-				Aegis::Renderer2D::SetFont(fonts_[0]);
-			}
-			else if (key_event->key_ == AE_KEY_G) {
-				Aegis::Renderer2D::SetFont(fonts_[1]);
-			}
-			static int x = 0;
-
-			if (key_event->key_ == AE_KEY_LEFT) {
-				--x_vel_;
-				--x;
-			}
-			else if (key_event->key_ == AE_KEY_RIGHT) {
-				++x;
-				++x_vel_;
-			}
-			if (key_event->key_ == AE_KEY_ENTER && key_event->action_ == AE_BUTTON_PRESS) {
-				manager_->ReplaceScene(std::unique_ptr<Aegis::Scene>(new MenuScene));
-			}
-
-			camera_.SetPosition({ (float)x, 0});
-		}
-
-		auto mouse_click = dynamic_cast<Aegis::MouseClickEvent*>(&event);
-		if (mouse_click && mouse_click->action_ == AE_BUTTON_PRESS) {
-			auto mouse_pos = Aegis::Application::GetWindow().GetMousePos();
-		}
 	}
 
 	void Render(float delta_time) override
 	{
 		Aegis::Renderer2D::BeginScene(camera_.view_projection_matrix_);
-
-		//Aegis::RenderSprite(*sprite_);
-		for (int y = 100; y < 110; ++y) {
-			for (int x = 100; x < 110; ++x) {
-				Aegis::DrawQuad({ x * 26.0f, y * 26.0f }, { 25.0f, 25.0f }, { 0.3f, 0.6f, 0.9f, 1.0f });
-				//Aegis::DrawQuad({ x * 2.0f, y * 2.0f }, { 1.0f, 1.0f }, smiley_);
-			}
-		}
-		//egis::DrawText("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", { 240, 240 }, { 1.0f, 0.0f, 1.0f, 1.0f });
-		//egis::DrawText("FPS: " + std::to_string(Aegis::Application::GetFrameTime()), { 0.0f,0.0f }, { 1.0, 1.0, 1.0, 1.0f });
-		//
-		//egis::DrawQuad({ x_pos_ + (x_vel_ * delta_time), 200.0f }, { 100.0f, 100.0f }, smiley_);
-		//egis::DrawQuad({ 400.0f, 200.0f }, { 100.0f, 100.0f }, container_);
+		
 		Aegis::Renderer2D::EndScene();
 	}
-	std::shared_ptr<Aegis::Texture> smiley_;
-	std::shared_ptr<Aegis::Texture> container_;
-	std::vector<std::shared_ptr<Aegis::Font>> fonts_;
-	
-	int x_vel_ = 0;
-	int accel_ = 2;
-	int x_pos_ = 0;
-	float time_step_ = 1.0 / 60.0f;
+
 };
 
 int main()
@@ -148,6 +39,5 @@ int main()
 	Aegis::Application::CreateWindow("Sandbox", 1280, 720);
 	Aegis::Application::ShowFrameTime(true);
 	Aegis::Application::PushScene(std::unique_ptr<Aegis::Scene>(new Sandbox));
-	Aegis::Application::PushScene(std::unique_ptr<Aegis::Scene>(new MenuScene));
 	Aegis::Application::Run();
 }
