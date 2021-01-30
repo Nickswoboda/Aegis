1c57b17be7f5a232b2743d015cb0658a1896928e
Nick Swoboda <nickswoboda@gmail.com>
2021-01-29 16:07:13 -0800

    Fix issue with FontMananger not retrieving from cache properly

--- src/FontManager.cpp
+++ src/FontManager.cpp
@@ -4,12 +4,13 @@ namespace Aegis{
 	std::unordered_map<std::string, std::shared_ptr<Font>> FontManager::fonts_;
 	std::shared_ptr<Font> FontManager::Load(const std::string& path, int size)
 	{
-		if (fonts_.find(path) != fonts_.end()) {
-			return fonts_[path];
+		std::string index = path + std::to_string(size);
+		if (fonts_.find(index) != fonts_.end()) {
+			return fonts_[index];
 		}
 
 		std::shared_ptr<Font> font = std::make_shared<Font>(path, size);
-		fonts_.emplace(path + std::to_string(size), font);
+		fonts_.emplace(index, font);
 
 		return font;
 	}
