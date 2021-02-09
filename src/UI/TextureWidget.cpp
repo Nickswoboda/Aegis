fd6566615715a171524ad98ac3a6cb31362b377e
Nick Swoboda <nickswoboda@gmail.com>
2021-02-08 20:45:27 -0800

    Add subtexture version of texturewidget

--- src/UI/TextureWidget.cpp
+++ src/UI/TextureWidget.cpp
@@ -2,22 +2,34 @@
 
 #include "../Renderer/Renderer.h"
 
-namespace Aegis{
-	TextureWidget::TextureWidget(std::shared_ptr<Texture> texture, const Vec2 pos, const Vec2 size, const Vec4& color)
-		: texture_(texture), color_(color), pos_(pos), size_(size) {};
-	
+namespace Aegis {
+	TextureWidgetBase::TextureWidgetBase(Vec2 pos, Vec2 size, Vec4 color)
+		:color_(color), pos_(pos), size_(size) {}
+	TextureWidget::TextureWidget(std::shared_ptr<Texture> texture, const Vec2 pos, const Vec4& color)
+		: TextureWidgetBase(pos, texture->size_, color), texture_(texture) {};
+
 	TextureWidget::TextureWidget(const Vec4& color, const Vec2 pos, const Vec2 size)
-		: color_(color), pos_(pos), size_(size) {};
+		: TextureWidgetBase(pos, size, color) {};
 
 	void TextureWidget::Render()
 	{
-		if (visible_){
-			if (texture_){
+		if (visible_) {
+			if (texture_) {
 				DrawQuad(pos_, size_, *texture_);
 			}
-			else{
+			else {
 				DrawQuad(pos_, size_, color_);
 			}
+		}
+	}
+
+	SubTextureWidget::SubTextureWidget(std::shared_ptr<SubTexture> subtexture, const Vec2 pos, const Vec4& color)
+		: TextureWidgetBase(pos, subtexture->size_, color), subtexture_(subtexture) {};
+
+	void SubTextureWidget::Render()
+	{
+		if (visible_) {
+			DrawQuad(pos_, *subtexture_);
 		}
 	}
 }
--- src/UI/TextureWidget.h
+++ src/UI/TextureWidget.h
@@ -3,19 +3,35 @@
 #include "Widget.h"
 
 namespace Aegis{
-	class TextureWidget : public Widget
+
+	class TextureWidgetBase : public Widget
 	{
 	public:
-
-		TextureWidget(std::shared_ptr<Texture> texture, const Vec2 pos, const Vec2 size, const Vec4& color = {1.0f, 1.0f, 1.0f, 1.0f});
-		TextureWidget(const Vec4& color, const Vec2 pos, const Vec2 size);
-
+		TextureWidgetBase(Vec2 pos, Vec2 size, Vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f });
 		void OnEvent(Event& event) override {};
-		void Render() override;
-
-		std::shared_ptr<Texture> texture_;
 		Vec4 color_;
 		Vec2 pos_;
 		Vec2 size_;
+	};
+
+	class TextureWidget : public TextureWidgetBase
+	{
+	public:
+
+		TextureWidget(std::shared_ptr<Texture> texture, const Vec2 pos, const Vec4& color = {1.0f, 1.0f, 1.0f, 1.0f});
+		TextureWidget(const Vec4& color, const Vec2 pos, const Vec2 size);
+
+		void Render() override;
+
+		std::shared_ptr<Texture> texture_;
+	};
+
+	class SubTextureWidget : public TextureWidgetBase
+	{
+	public:
+		SubTextureWidget(std::shared_ptr<SubTexture> subtexture, const Vec2 pos, const Vec4& color = { 1.0f, 1.0f, 1.0f, 1.0f });
+		void Render() override;
+
+		std::shared_ptr<SubTexture> subtexture_;
 	};
 }
