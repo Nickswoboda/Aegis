96ae47248e897487f3d241cc1971ee8856a5c0c0
Nick Swoboda <nickswoboda@gmail.com>
2021-03-12 19:35:19 -0800

    Fix: Fix rendering of upscaled images
    - Properly bind texture before applying texture parameters
    - Set texture min/mag parameters to nearest - should add setter for this
      in future

--- src/Renderer/Texture.cpp
+++ src/Renderer/Texture.cpp
@@ -65,10 +65,11 @@ namespace Aegis {
             format = GL_RGBA;
         }
 
+        glBindTextureUnit(0, ID_);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         glTextureStorage2D(ID_, 1, internal_format, width, height);
         glTextureSubImage2D(ID_, 0, 0, 0, width, height, format, GL_UNSIGNED_BYTE, data);
